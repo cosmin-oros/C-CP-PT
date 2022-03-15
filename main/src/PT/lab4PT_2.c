eb56f41e16f4a9417317770c4bdb2eb0cb1a6bfa
cosmin-oros <oroscosmin09@gmail.com>
2022-03-15 10:02:24 +0200

    update2lab4PT

--- main/src/PT/lab4PT_2.c
+++ main/src/PT/lab4PT_2.c
@@ -17,7 +17,7 @@ struct complex{
         circle = 1,
         triangle = 2,
         rectangle = 3
-    };                  //didn't use it
+    }geomFig;                  
     union{
         struct{
             float centerX;
@@ -64,7 +64,7 @@ struct complex{
 
 }geomObj[SIZE];
 
-void read_coords(struct complex* geomObj){
+unsigned read_coords_and_get_nrOfElem(struct complex* geomObj){ //one function should have only one purpose, but in this case I needed the nr of elem as well
     printf("-----------------------------------------------------------------------------------------------------------------------\n\n");
     printf("Welcome, this program can memorize the 2D cartezian coordinates of a maximum number of 1000 different geometric objects\n\n");
     printf("-----------------------------------------------------------------------------------------------------------------------\n");
@@ -80,17 +80,20 @@ void read_coords(struct complex* geomObj){
         }
         else if (key==1)
         {
+            geomObj[nrOfElements].geomFig=circle;
             printf("Enter the coordinates of the circle: center (x,y) and radius\n");
             scanf("(%f,%f) %f",&geomObj[nrOfElements].coords.circle.centerX,&geomObj[nrOfElements].coords.circle.centerY,&geomObj[nrOfElements].coords.circle.radius);
         }
         else if (key==2)
         {
+            geomObj[nrOfElements].geomFig=triangle;
             printf("Enter the coordinates of the triangle: P1 (x1,y1) P2 (x2,y2) P3 (x3,y3)\n");
             scanf("(%f,%f) (%f,%f) (%f,%f)",&geomObj[nrOfElements].coords.triangle.p1.x1,&geomObj[nrOfElements].coords.triangle.p1.y1,&geomObj[nrOfElements].coords.triangle.p2.x2,
             &geomObj[nrOfElements].coords.triangle.p2.y2,&geomObj[nrOfElements].coords.triangle.p3.x3,&geomObj[nrOfElements].coords.triangle.p3.y3);
         }
         else if (key==3)
         {
+            geomObj[nrOfElements].geomFig=rectangle;
             printf("Enter the coordinates of the rectangle: P1 (x1,y1) P2 (x2,y2) P3 (x3,y3) P4 (x4,y4)\n");
             scanf("(%f,%f) (%f,%f) (%f,%f) (%f,%f)",&geomObj[nrOfElements].coords.rectangle.p1.x1,&geomObj[nrOfElements].coords.rectangle.p1.y1,
             &geomObj[nrOfElements].coords.rectangle.p2.x2,&geomObj[nrOfElements].coords.rectangle.p2.y2,
@@ -99,15 +102,20 @@ void read_coords(struct complex* geomObj){
         }  
         nrOfElements++;
     }
+    return nrOfElements;
     
 }
 
-void write_coords(struct complex* geomObj){
-    //...
+void write_coords(struct complex* geomObj,unsigned n){
+    for (unsigned i = 0; i<n; i++)
+    {
+        //use   geomObj[i].geomFig
+    }
+    //do a program lab6PT_2_2.c where you use the function pointers correctly (two functions for each)
 }
 
 int main(){
-    read_coords(geomObj);
-    write_coords(geomObj);
+    int nrOfElem = read_coords_and_get_nrOfElem(geomObj);
+    write_coords(geomObj,nrOfElem);
     return 0;
 }
