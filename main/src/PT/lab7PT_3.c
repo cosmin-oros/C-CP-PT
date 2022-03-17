5a7cce26c84aa2757348e10fe2184cfddd8249b8
cosmin-oros <oroscosmin09@gmail.com>
2022-03-17 18:14:25 +0200

    lab7_3

---
+++ main/src/PT/lab7PT_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+/*
+3. Write a tool to convert from UNIX text files to DOS text files.
+UNIX uses newlines encoded by only one character "\n", and DOS
+uses two characters "\r\n". Use the stdin and stdout file streams.
+*/
+
+int main(){
+    const char* filename = "lab7PT_3.txt";
+    int c; 
+    FILE* fptr = fopen(filename,"r+");
+    while ((c = fgetc(fptr)) != EOF) { 
+        if (c == '\n'){ 
+            fputs("\r\n", fptr);
+        }  
+    }
+
+    return 0;
+}
