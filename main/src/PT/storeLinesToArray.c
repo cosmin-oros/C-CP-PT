510e743f5852d0725ce57be832f0400470999e55
cosmin-oros <oroscosmin09@gmail.com>
2022-05-08 17:02:26 +0300

    store lines from txt file to array

---
+++ main/src/PT/storeLinesToArray.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LSIZ 128 
+#define RSIZ 10 
+
+int main(void) 
+{
+    char line[RSIZ][LSIZ];
+	char fname[20];
+    FILE *fptr = NULL; 
+    int i = 0;
+    int tot = 0;
+    printf("\n\n Read the file and store the lines into an array :\n");
+	printf("------------------------------------------------------\n"); 
+	printf(" Input the filename to be opened : ");
+	scanf("%s",fname);	
+
+    fptr = fopen(fname, "r");
+    while(fgets(line[i], LSIZ, fptr)) 
+	{
+        line[i][strlen(line[i]) - 1] = '\0';
+        i++;
+    }
+    tot = i;
+	printf("\n The content of the file %s  are : \n",fname);    
+    for(i = 0; i < tot; ++i)
+    {
+        printf(" %s\n", line[i]);
+    }
+    printf("\n");
+    return 0;
+}
