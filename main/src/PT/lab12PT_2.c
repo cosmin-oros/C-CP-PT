bfaa9d9b9e1d58481aec9893f46c3380406baefd
cosmin-oros <oroscosmin09@gmail.com>
2022-05-20 18:06:33 +0300

    lab12 ex 2 split linked list into two DL Lists

---
+++ main/src/PT/lab12PT_2.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+2. Write a function to split a simple linked list into two double linked
+lists at a specific point in the original list. The specific point is
+provided by the key value.
+*/
+
+typedef struct Node{
+    int data;
+    struct Node* next;
+}Node;
+
+typedef struct DNode{
+    int data;
+    struct DNode* next;
+    struct DNode* prev;
+}DNode;
+
+void push(Node** head_ref,int new_data){
+    //allocate mem for new node
+    Node* new_node = (Node*)malloc(sizeof(Node));
+
+    new_node->data = new_data;
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+}
+
+//Function to print nodes in a given linked list
+void printList(Node* node)
+{
+    while(node!=NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+}
+
+void splitList(Node* head_ref,DNode** head_ref1, DNode** head_ref2, int key){
+    //insert the first 'key'-1 number of nodes in the first list (we eliminate the 'key' node because we split the list there)
+    while (key-1)
+    {
+        //allocate mem for new node
+        DNode* new_node = (DNode*)malloc(sizeof(DNode));
+
+        new_node->data = head_ref->data;
+        new_node->prev = NULL;
+        new_node->next = (*head_ref1);   
+        if((*head_ref1) !=  NULL){
+            (*head_ref1)->prev = new_node ;  
+        } 
+        (*head_ref1) = new_node;
+        head_ref = head_ref->next;
+
+        key--;
+    }
+
+    //skips over the Node where we split the original list
+    head_ref = head_ref->next;
+    
+    while (head_ref != NULL)
+    {
+        DNode* new_node = (DNode*)malloc(sizeof(DNode));
+
+        new_node->data = head_ref->data;
+        new_node->prev = NULL;
+        new_node->next = (*head_ref2);   
+        if((*head_ref2) !=  NULL){
+            (*head_ref2)->prev = new_node ;  
+        } 
+        (*head_ref2) = new_node;
+        head_ref = head_ref->next;
+    }
+    
+    
+}
+
+int main(){
+    //empty list
+    Node* head = NULL;
+    
+    //10->8->4->2
+    push(&head, 2);
+    push(&head, 4);
+    push(&head, 8);
+    push(&head, 10);
+    push(&head, 12);
+
+    printList(head);
+
+    int key = 3;
+    DNode* head1 = NULL;
+    DNode* head2 = NULL;
+
+    splitList(head,&head1,&head2,key);
+
+    printf("\nList1: ");
+    printList(head1);
+    printf("\nList2: ");
+    printList(head2);
+    printf("\n");
+
+    return 0;
+}
