889a0e6e10d8524e535b6113a477d7377f2bb0ff
cosmin-oros <oroscosmin09@gmail.com>
2022-05-21 19:19:18 +0300

    lab 12 PT

---
+++ main/src/PT/lab13PT_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long int fib(unsigned long int n){
+    // long int f[n+1];
+
+}
+
+int main(void){
+    printf("fib(10) = %lu\n",fib(10));
+    printf("fib(3) = %lu\n",fib(3));
+    printf("fib(5) = %lu\n",fib(5));
+    printf("fib(7) = %lu\n",fib(7));
+
+    return 0;
+}
