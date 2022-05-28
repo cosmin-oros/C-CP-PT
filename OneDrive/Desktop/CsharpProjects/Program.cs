e84e59c7f9c191f9618bf3435181f0b3691b30d5
cosmin-oros <oroscosmin09@gmail.com>
2022-05-29 00:10:56 +0300

    s

---
+++ OneDrive/Desktop/CsharpProjects/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Dice_Gane{
+    class Program{
+        static void main(string[] args){
+            int playerRandomNum;
+
+            Random random = new Random();
+
+            for(int i=0; i<10;i++){
+                Console.WriteLine("Press any key to roll the dice.");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1,7);
+                Console.WriteLine("You rolled a "+playerRandomNum);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
