ff99f786a304d4911d69ee7b081240bc7bbdb69e
cosmin-oros <oroscosmin09@gmail.com>
2022-05-28 17:07:03 +0300

    remove duplicates from linked list c++

---
+++ main/src/PT/removeDupsLinkedList.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+
+void printList(Node* head){
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << "\n";
+    
+}
+
+void push(Node** head_ref, int new_data)
+{
+    /* allocate node */
+    Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    if (!new_node)
+    {
+        cout << "Couldn't allocate mem\n";
+        exit(1);
+    }
+    
+    /* put in the data  */
+    new_node->data = new_data;
+                 
+    /* link the old list off the new node */
+    new_node->next = (*head_ref);    
+         
+    /* move the head to point to the new node */
+    (*head_ref) = new_node;
+}
+
+void removeDuplicates(Node* head){
+    Node* current = head;
+    Node* next_next;
+
+    //if the list is empty
+    if (current == NULL)
+    {
+        return;
+    }
+    
+    while (current->next != NULL)
+    {
+        if (current->data == current->next->data)
+        {
+            next_next = current->next->next;
+            free(current->next);
+            current->next = next_next;
+        }else{
+            current = current->next;
+        }
+        
+    }
+    
+}
+
+int main(){
+    /* Start with the empty list */
+    Node* head = NULL;
+   
+    /* Let us create a sorted linked list to test the functions
+     Created linked list will be 11->11->11->13->13->20 */
+    push(&head, 20);
+    push(&head, 13);
+    push(&head, 13); 
+    push(&head, 11);
+    push(&head, 11);
+    push(&head, 11);  
+
+    cout << "The list before removing the duplicates:\n";
+    printList(head);
+
+    removeDuplicates(head);
+
+    cout << "List after removing the duplicates:\n";
+    printList(head);
+
+    return 0;
+}
