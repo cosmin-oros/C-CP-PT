b742628dca5317358e80ac4609dd103c5fb98ecc
cosmin-oros <oroscosmin09@gmail.com>
2023-03-08 12:26:44 +0200

    lab3

--- main/src/PT/OS/lab3Assignment.sh
+++ main/src/PT/OS/lab3Assignment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# check if there are more than 3 arguments
 if [ $# -lt 4 ]; then
   echo "Usage: "
   for arg in "$@"
@@ -9,10 +10,15 @@ if [ $# -lt 4 ]; then
   exit 1
 fi
 
+# save the directory and the text file
+direc=$1
+file=$2
+
 shift 2
 
 count=0
 
+# count how many numbers are larger than 10
 for arg in "$@"
 do
 if [ $arg -gt 10 ]; then
@@ -24,6 +30,7 @@ echo $count
 
 sum=0
 
+# get the sum of all the arguments
 for arg in "$@"
 do
     (( sum += $arg ))
@@ -31,6 +38,22 @@ done
 
 echo $sum
 
-digits=0
+# print the number of digits of the sum
+echo -n $sum | wc -m
 
+# check if the file is a regular file
+if [ -f "$file" ]
+then
+  date=$(date +"%d-%m-%Y")
+  echo "$count $sum $date" >> "$file"
+else
+  echo "$file is not a regular file"
+fi
 
+# retrieve all the files with .txt entension and print the contents of them
+for txtFile in "$direc"/*.txt
+do
+  echo "Contents of $txtFile:"
+  cat "$txtFile"
+  echo ""
+done
