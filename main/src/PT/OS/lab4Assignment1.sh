41cc9924a3f3feeb2aee6befadeedc0d9562ca5d
cosmin-oros <oroscosmin09@gmail.com>
2023-03-15 09:53:52 +0200

    lab4 os

--- main/src/PT/OS/lab4Assignment1.sh
+++ main/src/PT/OS/lab4Assignment1.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-cond=0
-
-while [ $cond -lt 10 ]; do
-    read -p "Input: " text
-if echo "$text" | grep -q '[A-Z]'; then
+while read text 
+do
+    
+if echo "$text" | grep '[A-Z]'; then
   echo "Text contains uppercase letters."
 fi
 
-if echo "$text" | grep -q '[a-z]'; then
+if echo "$text" | grep '[a-z]'; then
   echo "Text contains lowercase letters."
 fi
 
-if echo "$text" | grep -q '[0-9]'; then
+if echo "$text" | grep '[0-9]'; then
   echo "Text contains digits."
 fi
 
-if echo "$text" | grep -q ' '; then
+if echo "$text" | grep ' '; then
   echo "Text contains spaces."
 fi
 
-if echo "$text" | grep -q ','; then
+if echo "$text" | grep ','; then
   echo "Text contains commas."
 fi
 
-if echo "${text:0:1}" | grep -q '[A-Z]'; then
+if echo "${text:0:1}" | grep '[A-Z]'; then
   echo "Text starts with upper case."
 fi
 
