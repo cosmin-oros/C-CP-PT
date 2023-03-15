aca0ec08014e00bd935fd4cbfdb7eff3d24f15e7
cosmin-oros <oroscosmin09@gmail.com>
2023-03-15 18:00:44 +0200

    lab4 OS

--- main/src/PT/OS/lab4Assignment1.sh
+++ main/src/PT/OS/lab4Assignment1.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-while read text 
-do
-    
-if echo "$text" | grep '[A-Z]'; then
-  echo "Text contains uppercase letters."
-fi
-
-if echo "$text" | grep '[a-z]'; then
-  echo "Text contains lowercase letters."
-fi
-
-if echo "$text" | grep '[0-9]'; then
-  echo "Text contains digits."
-fi
-
-if echo "$text" | grep ' '; then
-  echo "Text contains spaces."
-fi
-
-if echo "$text" | grep ','; then
-  echo "Text contains commas."
-fi
-
-if echo "${text:0:1}" | grep '[A-Z]'; then
-  echo "Text starts with upper case."
-fi
-
+while read text; do
+    # check if the text contains uppercase letters, lowercase letters, and digits.
+    if [[ "$text" =~ [A-Z] && "$text" =~ [a-z] && "$text" =~ [0-9] ]]; then
+        # check if the text contains spaces and commas.
+        if [[ "$text" =~ [\ ,] ]]; then
+            # check if the text starts with an uppercase letter.
+            if [[ "${text:0:1}" =~ [A-Z] ]]; then
+                # check if the text ends with a single period.
+                if [[ "${text: -1}" == "." && "${text: -2}" != ".." ]]; then
+                    # check if the text does not contain "and" followed by a comma and any space between them.
+                    if ! [[ "$text" =~ [Aa][Nn][Dd],\ *[A-Za-z] ]]; then
+                        # check if the text does not contain "np" or "nb".
+                        if ! [[ "$text" =~ [Nn][Pp,Bb] ]]; then
+                            echo "$text meets the requirements"
+                        fi
+                    fi
+                fi
+            fi
+        fi
+    fi
 done
 
